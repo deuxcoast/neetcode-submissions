1ca9808a19831b575d7eab3b392543bdd1176587
conor <conor.ux@gmail.com>
2026-09-24 10:45:34 -0700

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            signature = "".join(sorted(word))
+            anagram_map[signature].append(word)
+        
+        return [x for x in anagram_map.values()]
